5bb3c52ba5605f0cc15bd5799be73b5e3e63ea4c
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2024-10-28 18:14:40 +0400

    Finish day 4 practice 7

---
+++ Day4/Practice7/Practice7/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter a number: ");
+int num = Int32.Parse(Console.ReadLine());
+int num1 = 0;
+int num2 = 1;
+int sum;
+
+
+for (int i = 1; i <= num; i++)
+{
+   Console.Write(num1 + ", ");
+   sum = num1 + num2;
+   num1 = num2;
+   num2 = sum;
+
+}
