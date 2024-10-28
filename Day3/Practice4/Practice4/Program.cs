b4eb09e4e9311ad63359bfb0a5cef1419f0a8754
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2024-10-28 16:35:00 +0400

    finish day 3 practice 4

---
+++ Day3/Practice4/Practice4/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Enter your birth year: ");
+int year = Int32.Parse(Console.ReadLine());
+
+switch (year % 12)
+{
+    case 0:
+        Console.WriteLine(year + " was Monkey year");
+        break;
+    case 1:
+        Console.WriteLine(year + " was Rooster year");
+        break;
+    case 2:
+        Console.WriteLine(year + " was Dog year");
+        break;
+    case 3:
+        Console.WriteLine(year + " was Pig year");
+        break;
+    case 4:
+        Console.WriteLine(year + " was Rat year");
+        break;
+    case 5:
+        Console.WriteLine(year + " was Ox year");
+        break;
+    case 6:
+        Console.WriteLine(year + " was Tiger year");
+        break;
+    case 7:
+        Console.WriteLine(year + " was Rabbit year");
+        break;
+    case 8:
+        Console.WriteLine(year + " was Dragon year");
+        break;
+    case 9:
+        Console.WriteLine(year + " was Snake year");
+        break;
+    case 10:
+        Console.WriteLine(year + " was Horse year");
+        break;
+    case 11:
+        Console.WriteLine(year + " was Sheep year");
+        break;
+}
