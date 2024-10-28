8f6df463d987fa5fa12e1b3873381c98548597c5
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2024-10-28 17:11:33 +0400

    finish day 4 practice 3

---
+++ Day4/Practice3/Practice3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter a number: ");
+int num = Int32.Parse(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+    Console.WriteLine("num cubed is: " + Math.Pow(i, 3));
+}
