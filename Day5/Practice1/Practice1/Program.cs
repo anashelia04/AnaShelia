9ab70cb813b1538bcc71413111811a069cfaac5c
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2024-10-30 14:57:53 +0400

    finish day 5 practice 1

---
+++ Day5/Practice1/Practice1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter array size: ");
+int arraySize = Int32.Parse(Console.ReadLine());
+
+int[] nums = new int[arraySize];
+
+for (int i = 0; i < arraySize; i++)
+{
+    Console.WriteLine("Enter number for index " + i);
+    nums[i] = Int32.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Here is your array!");
+
+foreach (int i in nums)
+{
+    Console.WriteLine(i);
+    Console.WriteLine();
+}
