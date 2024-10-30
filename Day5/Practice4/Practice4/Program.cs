821ddfef72e29b3286ea1fd909cc7e0b2dbfa6da
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2024-10-30 15:07:37 +0400

    finish day 5 practice 4

---
+++ Day5/Practice4/Practice4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Enter array size: " + " ");
+int arraySize = Int32.Parse(Console.ReadLine());
+
+int[] nums = new int[arraySize];
+
+for (int i = 0; i < arraySize; i++)
+{
+    Console.Write("Enter number for index " + i + " : ");
+    nums[i] = Int32.Parse(Console.ReadLine());
+}
+
+int product = 1;
+foreach (int i in nums)
+{
+    product = product * i;
+}
+
+Console.WriteLine("Product of array elements is " + product);
