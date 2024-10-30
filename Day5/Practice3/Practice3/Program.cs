48cfc31915fb0ba24953695b32cc58c2e4e316aa
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2024-10-30 15:05:30 +0400

    finish day 5 practice 3

---
+++ Day5/Practice3/Practice3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Enter array size: " + " ");
+int arraySize = Int32.Parse(Console.ReadLine());
+
+int[] nums = new int[arraySize];
+
+for (int i = 0; i < arraySize; i++)
+{
+    Console.Write("Enter number for index " + i + " : ");
+    nums[i] = Int32.Parse(Console.ReadLine());
+}
+
+int sum = 0;
+foreach (int i in nums)
+{
+    sum = sum + i;
+}
+
+Console.WriteLine("Sum of array elements is " + sum);
