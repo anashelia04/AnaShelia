a1138c9081f84ff60652f1b727d350b0c06f184f
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2024-11-01 17:45:03 +0400

    finish day 6 practice 1

---
+++ Day6/Practice1/Practice1/Program.cs
@@ -0,0 +1,8 @@
+﻿static int ArrayIndex(int[] numsArray, int index)
+{
+    int num = numsArray[index];
+    return num;
+}
+
+int[] numsArray = { 1, 3, 4, 15, 13, 23, 98 };
+Console.WriteLine("Number at index " + 0 + " in the array is " + ArrayIndex(numsArray, 0));
