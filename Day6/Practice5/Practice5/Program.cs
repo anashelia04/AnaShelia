e72afef911cfeb864374a7962cc4958ac1c592ad
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2024-11-01 17:57:04 +0400

    finish day 6 practice 5

---
+++ Day6/Practice5/Practice5/Program.cs
@@ -0,0 +1,45 @@
+﻿static int[] CreateArray()
+{
+    Console.Write("Enter size of array: ");
+    int size = Int32.Parse(Console.ReadLine());
+    int[] numsArray = new int[size];
+
+    for (int i = 0; i < numsArray.Length; i++)
+    {
+        Console.Write("Enter integer for index " + i + ": ");
+        numsArray[i] = Int32.Parse(Console.ReadLine());
+    }
+    return numsArray;
+}
+
+static int Factorial(int number)
+{
+    if (number == 0)
+    {
+        return 1;
+    }
+    return number * Factorial(number - 1);
+}
+
+static int ArrayElementFactorial(int[] nums, int number)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] == number)
+        {
+            return Factorial(number);
+        }
+    }
+    return -1;
+}
+
+int[] numsArray = CreateArray();
+int factorial = ArrayElementFactorial(numsArray, 6);
+if (factorial == -1)
+{
+    Console.WriteLine("Number was not found in the given array");
+}
+else
+{
+    Console.WriteLine("Factorial is " + factorial);
+}
