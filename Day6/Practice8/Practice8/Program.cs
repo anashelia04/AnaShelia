95241e63535c0019343eafa2692662a2aad5f950
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2024-11-01 18:07:10 +0400

    finish day 6 practice 8

---
+++ Day6/Practice8/Practice8/Program.cs
@@ -0,0 +1,26 @@
+﻿static int MyNumber()
+{
+    Console.Write("Enter a positive number: ");
+    int num = Int32.Parse(Console.ReadLine());
+
+    return num;
+}
+
+static void SplitNumber(int num)
+{
+    int power = 0;
+    int initialNumber = num;
+    string answer = "";
+    while (num != 0)
+    {
+        int lastDigit = num % 10;
+        answer = lastDigit + " * 10^" + power + " + " + answer;
+        power++;
+        num = num / 10;
+    }
+    Console.WriteLine(initialNumber + " = " + answer + "0");
+}
+
+int num = MyNumber();
+SplitNumber(num);
+
