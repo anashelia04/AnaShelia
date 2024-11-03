35eea83618137fa1a26c7962ae88f2912538d147
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2024-11-04 03:15:01 +0400

    finish day 7 practice 3

---
+++ Day7/practice3/practice3/Program.cs
@@ -0,0 +1,14 @@
+﻿static void SpacedString(string text)
+{
+    char[] chars = text.ToCharArray();
+    string newString = "";
+    for (int i = 0; i < chars.Length; i++)
+    {
+        newString = newString + chars[i] + " ";
+    }
+    Console.WriteLine(newString);
+}
+
+Console.WriteLine("Enter word: ");
+string enteredString = Console.ReadLine().ToUpper();
+SpacedString(enteredString);
