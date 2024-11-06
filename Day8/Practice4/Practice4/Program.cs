6f31d257104b16734811c5c6a052f252f38b6c81
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2024-11-06 17:41:01 +0400

    finish day 8 practice 4

---
+++ Day8/Practice4/Practice4/Program.cs
@@ -0,0 +1,11 @@
+﻿static int MyMethod(int number, int degree)
+{
+    if (degree == 0){
+        return 1;
+    }
+
+    return number * MyMethod(number, degree - 1);
+}
+
+int answer = MyMethod(5, 3);
+Console.WriteLine(answer);
