ac140157a3eaf00b3de83e7216c5913b454d3e22
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2024-11-08 18:22:36 +0400

    finish day 9 practice 2

---
+++ Day9/Practice2/Practice2/Program.cs
@@ -0,0 +1,15 @@
+﻿using Practice2;
+
+Console.Write("Enter side 1: ");
+double side1 = Convert.ToSingle(Console.ReadLine());
+Console.Write("Enter side 2: ");
+double side2 = Convert.ToSingle(Console.ReadLine());
+Console.Write("Enter side 3: ");
+double side3 = Convert.ToSingle(Console.ReadLine());
+
+Triangle triangle = new Triangle();
+triangle.Side1 = side1;
+triangle.Side2 = side2;
+triangle.Side3 = side3;
+Console.WriteLine($"Perimeter of the triangle is: {triangle.Perimeter()}");
+Console.WriteLine($"Area of the triangle is: {triangle.Area()}");
