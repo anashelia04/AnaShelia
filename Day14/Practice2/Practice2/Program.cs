04e8c8e520c7118eef82cc02d26f3b4cf26c331b
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2024-11-25 16:35:54 +0400

    finish day 14 practice 2

---
+++ Day14/Practice2/Practice2/Program.cs
@@ -0,0 +1,31 @@
+﻿public class GenericMaxFinder
+{
+    public static T FindMaximum<T>(T[] array) where T : IComparable<T>
+    {
+        if (array == null || array.Length == 0)
+        {
+            return default; 
+        }
+
+        T max = array[0];
+        foreach (T item in array)
+        {
+            if (item.CompareTo(max) > 0)
+            {
+                max = item;
+            }
+        }
+        return max;
+    }
+
+    public static void Main(string[] args)
+    {   
+        string[] words = { "apple", "banana", "cherry" };
+        int[] numbers = { 13, 5, 20, 1, 99 };
+
+        Console.WriteLine("Max in words: " + FindMaximum(words));
+        Console.WriteLine("Max in numbers: " + FindMaximum(numbers));
+
+
+    }
+}
