2f5eca5cc6df90e09940ed4267b028cc044f8ea1
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2024-11-28 22:08:07 +0400

    finish day 15 practice 1

---
+++ Day15/practice1/practice1/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.WriteLine("Enter a string of brackets ");
+string input = Console.ReadLine();
+
+bool isValid = IsValidString(input);
+Console.WriteLine(isValid ? "The string is valid." : "The string is invalid.");
+static bool IsValidString(string input)
+{
+    if (input.Length == 0)
+    {
+        return false;
+    }
+    var left = new[] { '(', '{', '[' };
+    var right = new[] { ')', '}', ']' };
+    var stack = new Stack<char>();
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (left.Contains(input[i]))
+        {
+            stack.Push(input[i]);
+            continue;
+        }
+
+        if (right.Contains(input[i])) 
+        {
+            if (!stack.Any())
+            {
+                return false;
+            }
+
+            var x = stack.Peek() - input[i];
+
+            if (x < 0 && x >= -2)
+            {
+                stack.Pop();
+                continue;
+            }
+            return false;
+        }
+    }
+
+    if (stack.Any())
+    {
+        return false;
+    }
+    return true;
+}
+
