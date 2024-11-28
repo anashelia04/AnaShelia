59787b5c2078153e6fbb58dca456bb90203597eb
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2024-11-28 22:43:19 +0400

    finish day 15 practice 3

---
+++ Day15/Practice3/Practice3/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice3
+{
+    public class Book
+    {
+        public string FullName { get; set; }
+        public int ReleaseYear { get; set; }
+        public string BookName { get; set; }
+        public string ISBN { get; set; }
+        public Genre BookGenre { get; set; }
+        public Book( string bookName, string fullName, int releaseYear, Genre bookGenre, string isbn)
+        {
+            this.FullName = fullName;
+            this.BookName = bookName;
+            this.ReleaseYear = releaseYear;
+            this.ISBN = isbn;
+            this.BookGenre = bookGenre;
+        }
+        public enum Genre
+        {
+            Fantasy,
+            Fiction,
+            Horror,
+            Mystery,
+            Romance,
+            Thriller,
+            Realism
+        }
+
+    }
+}
---
+++ Day15/Practice3/Practice3/ReleaseYearComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice3
+{
+    public class ReleaseYearComparer : IComparer<Book>
+    {
+        public int Compare(Book? x, Book? y)
+        {
+            return x.ReleaseYear.CompareTo(y.ReleaseYear);
+        }
+
+    }
+}
