d47ea6cc5e04732627dd0a52e99b95ae8dfa15f8
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2024-12-02 18:22:41 +0400

    finish day 16 practice 1

---
+++ Day16/Practice1/Practice1/DebitIBAN.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    public class DebitIBAN : IBAN
+    {
+        public DebitIBAN(string accountNumber, decimal initialBalance) : base(accountNumber, initialBalance) { }
+
+        public override void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+                throw new AmountException("Deposit amount must be greater than zero.");
+
+            Balance += amount;
+        }
+
+        public override void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+                throw new AmountException("Withdrawal amount must be greater than zero.");
+
+            if (Balance - amount < 0)
+                throw new AmountException("Insufficient balance.");
+
+            Balance -= amount;
+        }
+    }
+
+}
---
+++ Day16/Practice1/Practice1/ExceptionHandling.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    public class ExceptionHandling
+    {
+        public static string GetLastInnerExMessage(Exception ex)
+        {
+            while (ex.InnerException != null)
+            {
+                ex = ex.InnerException;
+            }
+            return ex.Message;
+        }
+
+        public static string GetAllInnerExMessageTogether(Exception ex)
+        {
+            var messages = new List<string>();
+
+            while (ex != null)
+            {
+                messages.Add(ex.Message);
+                ex = ex.InnerException;
+            }
+
+            return string.Join("; ", messages);
+        }
+    }
+
+}
