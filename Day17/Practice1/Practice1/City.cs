6501c511347e0586396ac5870be478ec9136f561
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2024-12-04 23:10:39 +0400

    finish day 17 practice 1

---
+++ Day17/Practice1/Practice1/City.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    public class City : GeographicEntity
+    {
+
+        public string CountryName;
+
+        public bool IsCapital;
+        public City(string name, double area, int population, bool isCapital, string countryName)
+        {
+            Name = name;
+            Area = area;
+            Population = population;
+            CountryName = countryName;
+            IsCapital = isCapital;
+        }
+
+        public override string ToString()
+        {
+            string capitalStatus = IsCapital ? "(Capital)" : "";
+            return $"City: {Name}\nArea: {Area} \nPopulation: {Population} \nCapital: {IsCapital} \nCountry: {CountryName}";
+        }
+    }
+
+}
+
---
+++ Day17/Practice1/Practice1/SingleCapitalException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    public class SingleCapitalException : Exception
+    {
+        public SingleCapitalException(string message) : base(message)
+        {
+            Console.WriteLine("The country must have only one capital");
+        }
+    }
+}
+
