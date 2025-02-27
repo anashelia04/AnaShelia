452303c7796d70a8e85e070a18ce35a4f93362c4
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2025-02-27 20:09:11 +0400

    finish day 28-30 practice

---
+++ Day 28-30/WebApplication1/WebApplication1/Controllers/BooksController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Interfaces;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookService _bookService;
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        {
+            return Ok(_bookService.GetBooks());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBook(int id)
+        {
+            var book = _bookService.GetBook(id);
+            if (book == null)
+                return NotFound();
+            return Ok(book);
+        }
+    }
+}
---
+++ Day 28-30/WebApplication1/WebApplication1/Services/BookService.cs
@@ -0,0 +1,18 @@
+﻿using WebApplication1.Interfaces;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly List<Book> books = new()
+    {
+        new Book { Id = 1, Title = "The Catcher in the Rye", Author = "J.D. Salinger", YearPublished = 1951 },
+        new Book { Id = 2, Title = "Harry Potter and the Sorcerer's Stone", Author = "J.K. Rowling", YearPublished = 1997 },
+        new Book { Id = 3, Title = "Harry Potter and the Half Blood Prince", Author = "J.K. Rowling", YearPublished = 2005 }
+    };
+
+        public IEnumerable<Book> GetBooks() => books;
+        public Book GetBook(int id) => books.FirstOrDefault(b => b.Id == id);
+    }
+}
